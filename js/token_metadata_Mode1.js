54297e8443021d8cb0a79fc15db495fa1f35d580
zeroheight <tokens-management@zeroheight.com>
2026-02-17 14:48:22 -0600

    Update MUI 7.2-vanilla-test2 from zeroheight

---
+++ js/token_metadata_Mode1.js
@@ -0,0 +1,34 @@
+module.exports = {
+  "content": {
+    "material-ui": {
+      "value": "7.2.0",
+      "original": {
+        "value": "7.2.0"
+      },
+      "name": "ContentMaterialUi",
+      "attributes": {
+        "category": "content",
+        "type": "material-ui"
+      },
+      "path": [
+        "content",
+        "material-ui"
+      ]
+    },
+    "mui-x": {
+      "value": "8.10.0",
+      "original": {
+        "value": "8.10.0"
+      },
+      "name": "ContentMuiX",
+      "attributes": {
+        "category": "content",
+        "type": "mui-x"
+      },
+      "path": [
+        "content",
+        "mui-x"
+      ]
+    }
+  }
+};
