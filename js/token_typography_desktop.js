d1d5ebef450242b926d9e28f484286a5de32d80d
zeroheight <tokens-management@zeroheight.com>
2026-02-17 14:49:17 -0600

    Update MUI 7.2-vanilla-test2 from zeroheight

---
+++ js/token_typography_desktop.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "content": {
+    "fontFamily": {
+      "value": "Montserrat",
+      "original": {
+        "value": "Montserrat"
+      },
+      "name": "ContentFontFamily",
+      "attributes": {
+        "category": "content",
+        "type": "fontFamily"
+      },
+      "path": [
+        "content",
+        "fontFamily"
+      ]
+    }
+  }
+};
