006fe0643f8d89ea202a2b19ba1efaf1c070e2ae
zeroheight <tokens-management@zeroheight.com>
2026-03-12 13:38:27 -0500

    Update [DS] - Centro 1.0.0 from zeroheight

--- js/token_typography_mobile.js
+++ js/token_typography_mobile.js
@@ -1,5 +1,99 @@
 module.exports = {
   "content": {
+    "caption": {
+      "font-weight": {
+        "value": "SemiBold",
+        "original": {
+          "value": "SemiBold"
+        },
+        "name": "ContentCaptionFontWeight",
+        "attributes": {
+          "category": "content",
+          "type": "caption",
+          "item": "font-weight"
+        },
+        "path": [
+          "content",
+          "caption",
+          "font-weight"
+        ]
+      }
+    },
+    "font-weight-bold": {
+      "value": "Bold",
+      "original": {
+        "value": "Bold"
+      },
+      "name": "ContentFontWeightBold",
+      "attributes": {
+        "category": "content",
+        "type": "font-weight-bold"
+      },
+      "path": [
+        "content",
+        "font-weight-bold"
+      ]
+    },
+    "font-weight-light": {
+      "value": "Light",
+      "original": {
+        "value": "Light"
+      },
+      "name": "ContentFontWeightLight",
+      "attributes": {
+        "category": "content",
+        "type": "font-weight-light"
+      },
+      "path": [
+        "content",
+        "font-weight-light"
+      ]
+    },
+    "font-weight-medium": {
+      "value": "Medium",
+      "original": {
+        "value": "Medium"
+      },
+      "name": "ContentFontWeightMedium",
+      "attributes": {
+        "category": "content",
+        "type": "font-weight-medium"
+      },
+      "path": [
+        "content",
+        "font-weight-medium"
+      ]
+    },
+    "font-weight-regular": {
+      "value": "Regular",
+      "original": {
+        "value": "Regular"
+      },
+      "name": "ContentFontWeightRegular",
+      "attributes": {
+        "category": "content",
+        "type": "font-weight-regular"
+      },
+      "path": [
+        "content",
+        "font-weight-regular"
+      ]
+    },
+    "font-weight-semibold": {
+      "value": "SemiBold",
+      "original": {
+        "value": "SemiBold"
+      },
+      "name": "ContentFontWeightSemibold",
+      "attributes": {
+        "category": "content",
+        "type": "font-weight-semibold"
+      },
+      "path": [
+        "content",
+        "font-weight-semibold"
+      ]
+    },
     "fontFamily": {
       "value": "Montserrat",
       "original": {
@@ -14,6 +108,326 @@ module.exports = {
         "content",
         "fontFamily"
       ]
+    },
+    "fontFamilyMonospace": {
+      "value": "Montserrat",
+      "original": {
+        "value": "Montserrat"
+      },
+      "name": "ContentFontFamilyMonospace",
+      "attributes": {
+        "category": "content",
+        "type": "fontFamilyMonospace"
+      },
+      "path": [
+        "content",
+        "fontFamilyMonospace"
+      ]
+    },
+    "header 1": {
+      "font-family": {
+        "value": "Montserrat",
+        "original": {
+          "value": "Montserrat"
+        },
+        "name": "ContentHeader1FontFamily",
+        "attributes": {
+          "category": "content",
+          "type": "header 1",
+          "item": "font-family"
+        },
+        "path": [
+          "content",
+          "header 1",
+          "font-family"
+        ]
+      },
+      "font-weight": {
+        "value": "SemiBold",
+        "original": {
+          "value": "SemiBold"
+        },
+        "name": "ContentHeader1FontWeight",
+        "attributes": {
+          "category": "content",
+          "type": "header 1",
+          "item": "font-weight"
+        },
+        "path": [
+          "content",
+          "header 1",
+          "font-weight"
+        ]
+      }
+    },
+    "header 2": {
+      "font-family": {
+        "value": "Montserrat",
+        "original": {
+          "value": "Montserrat"
+        },
+        "name": "ContentHeader2FontFamily",
+        "attributes": {
+          "category": "content",
+          "type": "header 2",
+          "item": "font-family"
+        },
+        "path": [
+          "content",
+          "header 2",
+          "font-family"
+        ]
+      },
+      "font-weight": {
+        "value": "SemiBold",
+        "original": {
+          "value": "SemiBold"
+        },
+        "name": "ContentHeader2FontWeight",
+        "attributes": {
+          "category": "content",
+          "type": "header 2",
+          "item": "font-weight"
+        },
+        "path": [
+          "content",
+          "header 2",
+          "font-weight"
+        ]
+      }
+    },
+    "header 3": {
+      "font-family": {
+        "value": "Montserrat",
+        "original": {
+          "value": "Montserrat"
+        },
+        "name": "ContentHeader3FontFamily",
+        "attributes": {
+          "category": "content",
+          "type": "header 3",
+          "item": "font-family"
+        },
+        "path": [
+          "content",
+          "header 3",
+          "font-family"
+        ]
+      },
+      "font-weight": {
+        "value": "SemiBold",
+        "original": {
+          "value": "SemiBold"
+        },
+        "name": "ContentHeader3FontWeight",
+        "attributes": {
+          "category": "content",
+          "type": "header 3",
+          "item": "font-weight"
+        },
+        "path": [
+          "content",
+          "header 3",
+          "font-weight"
+        ]
+      }
+    },
+    "header 4": {
+      "font-family": {
+        "value": "Montserrat",
+        "original": {
+          "value": "Montserrat"
+        },
+        "name": "ContentHeader4FontFamily",
+        "attributes": {
+          "category": "content",
+          "type": "header 4",
+          "item": "font-family"
+        },
+        "path": [
+          "content",
+          "header 4",
+          "font-family"
+        ]
+      },
+      "font-weight": {
+        "value": "SemiBold",
+        "original": {
+          "value": "SemiBold"
+        },
+        "name": "ContentHeader4FontWeight",
+        "attributes": {
+          "category": "content",
+          "type": "header 4",
+          "item": "font-weight"
+        },
+        "path": [
+          "content",
+          "header 4",
+          "font-weight"
+        ]
+      }
+    },
+    "header 5": {
+      "font-family": {
+        "value": "Montserrat",
+        "original": {
+          "value": "Montserrat"
+        },
+        "name": "ContentHeader5FontFamily",
+        "attributes": {
+          "category": "content",
+          "type": "header 5",
+          "item": "font-family"
+        },
+        "path": [
+          "content",
+          "header 5",
+          "font-family"
+        ]
+      },
+      "font-weight": {
+        "value": "SemiBold",
+        "original": {
+          "value": "SemiBold"
+        },
+        "name": "ContentHeader5FontWeight",
+        "attributes": {
+          "category": "content",
+          "type": "header 5",
+          "item": "font-weight"
+        },
+        "path": [
+          "content",
+          "header 5",
+          "font-weight"
+        ]
+      }
+    },
+    "header 6": {
+      "font-family": {
+        "value": "Montserrat",
+        "original": {
+          "value": "Montserrat"
+        },
+        "name": "ContentHeader6FontFamily",
+        "attributes": {
+          "category": "content",
+          "type": "header 6",
+          "item": "font-family"
+        },
+        "path": [
+          "content",
+          "header 6",
+          "font-family"
+        ]
+      },
+      "font-weight": {
+        "value": "SemiBold",
+        "original": {
+          "value": "SemiBold"
+        },
+        "name": "ContentHeader6FontWeight",
+        "attributes": {
+          "category": "content",
+          "type": "header 6",
+          "item": "font-weight"
+        },
+        "path": [
+          "content",
+          "header 6",
+          "font-weight"
+        ]
+      }
+    },
+    "monospace": {
+      "font-family": {
+        "value": "Montserrat",
+        "original": {
+          "value": "Montserrat"
+        },
+        "name": "ContentMonospaceFontFamily",
+        "attributes": {
+          "category": "content",
+          "type": "monospace",
+          "item": "font-family"
+        },
+        "path": [
+          "content",
+          "monospace",
+          "font-family"
+        ]
+      },
+      "font-weight": {
+        "value": "Regular",
+        "original": {
+          "value": "Regular"
+        },
+        "name": "ContentMonospaceFontWeight",
+        "attributes": {
+          "category": "content",
+          "type": "monospace",
+          "item": "font-weight"
+        },
+        "path": [
+          "content",
+          "monospace",
+          "font-weight"
+        ]
+      }
+    },
+    "paragraph": {
+      "paragraph-weight": {
+        "value": "Regular",
+        "original": {
+          "value": "Regular"
+        },
+        "name": "ContentParagraphParagraphWeight",
+        "attributes": {
+          "category": "content",
+          "type": "paragraph",
+          "item": "paragraph-weight"
+        },
+        "path": [
+          "content",
+          "paragraph",
+          "paragraph-weight"
+        ]
+      },
+      "paragraph-weight-bold": {
+        "value": "Bold",
+        "original": {
+          "value": "Bold"
+        },
+        "name": "ContentParagraphParagraphWeightBold",
+        "attributes": {
+          "category": "content",
+          "type": "paragraph",
+          "item": "paragraph-weight-bold"
+        },
+        "path": [
+          "content",
+          "paragraph",
+          "paragraph-weight-bold"
+        ]
+      },
+      "paragraph-weight-medium": {
+        "value": "Medium",
+        "original": {
+          "value": "Medium"
+        },
+        "name": "ContentParagraphParagraphWeightMedium",
+        "attributes": {
+          "category": "content",
+          "type": "paragraph",
+          "item": "paragraph-weight-medium"
+        },
+        "path": [
+          "content",
+          "paragraph",
+          "paragraph-weight-medium"
+        ]
+      }
     }
   }
 };
